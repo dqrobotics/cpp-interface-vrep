d06387f39d232ae199c290c14a55085c9d3bd507
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2022-01-28 15:31:22 -0300

    [DQ_VrepInterface.h] Updated the Copyright.

--- include/dqrobotics/interfaces/vrep/DQ_VrepInterface.h
+++ include/dqrobotics/interfaces/vrep/DQ_VrepInterface.h
@@ -1,5 +1,5 @@
 ﻿/**
-(C) Copyright 2019 DQ Robotics Developers
+(C) Copyright 2022 DQ Robotics Developers
 
 This file is part of DQ Robotics.
 
@@ -18,6 +18,7 @@ This file is part of DQ Robotics.
 
 Contributors:
 - Murilo M. Marinho        (murilo@nml.t.u-tokyo.ac.jp)
+- Juan Jose Quiroz Omana   (juanjqo@g.ecc.u-tokyo.ac.jp)
 */
 
 #ifndef DQ_ROBOTICS_INTERFACE_VREP_HEADER_GUARD
