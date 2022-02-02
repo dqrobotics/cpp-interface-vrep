f3941870931da60c1ebf8ff72abdda3d3d262219
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2022-02-02 15:14:42 -0300

    [DQ_VrepInterface.h] Added overloaded methods for get_inertia_matrix, get_center_of_mass and get_mass.

--- include/dqrobotics/interfaces/vrep/DQ_VrepInterface.h
+++ include/dqrobotics/interfaces/vrep/DQ_VrepInterface.h
@@ -287,10 +287,13 @@ public:
 
 
     MatrixXd get_inertia_matrix(const std::string& link_name, const std::string& reference_frame="shape_frame", const std::string& function_name = "get_inertia", const std::string& obj_name= "DQRoboticsApiCommandServer");
+    MatrixXd get_inertia_matrix(const int& handle, const std::string& reference_frame="shape_frame", const std::string& function_name = "get_inertia", const std::string& obj_name= "DQRoboticsApiCommandServer");
 
     VectorXd get_center_of_mass(const std::string& link_name, const std::string& reference_frame="shape_frame", const std::string& function_name = "get_center_of_mass", const std::string& obj_name= "DQRoboticsApiCommandServer");
+    VectorXd get_center_of_mass(const int& handle, const std::string& reference_frame="shape_frame", const std::string& function_name = "get_center_of_mass", const std::string& obj_name= "DQRoboticsApiCommandServer");
 
     double get_mass(const std::string& link_name, const std::string& function_name = "get_mass", const std::string& obj_name= "DQRoboticsApiCommandServer");
+    double get_mass(const int& handle, const std::string& function_name = "get_mass", const std::string& obj_name= "DQRoboticsApiCommandServer");
 
 
 
